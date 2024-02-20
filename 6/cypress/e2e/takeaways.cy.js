793c6ca9008c67b8b412ce23c4d6f7ab6ffb3a57
Yevhenii Zhyrov <yevheniizhyrov.dev@gmail.com>
2024-02-20 13:06:04 +0200

    6. Seed test db

--- 6/cypress/e2e/takeaways.cy.js
+++ 6/cypress/e2e/takeaways.cy.js
@@ -1,6 +1,9 @@
 /// <reference types="Cypress" />
 
 describe("Takeaways", () => {
+  beforeEach(() => {
+    cy.task("seedDatabase");
+  });
   it("should display a list of fetched takeaways", () => {
     cy.visit("/");
     cy.get('[data-cy="takeaway-item"]').should("have.length", 2);
