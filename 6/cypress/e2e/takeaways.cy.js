d3fb4ca4702887736503d2d998064c9da5c14e6d
Yevhenii Zhyrov <yevheniizhyrov.dev@gmail.com>
2024-02-20 14:27:35 +0200

    6. Add network takeaway test

--- 6/cypress/e2e/takeaways.cy.js
+++ 6/cypress/e2e/takeaways.cy.js
@@ -8,4 +8,15 @@ describe("Takeaways", () => {
     cy.visit("/");
     cy.get('[data-cy="takeaway-item"]').should("have.length", 2);
   });
+  it("should add a new takeaway", () => {
+    cy.intercept("POST", "/takeaways/new*", "success").as("createTakeaway");
+    cy.login();
+    cy.visit("/takeaways/new");
+    cy.get('[data-cy="title"]').type("TestTitle1");
+    cy.get('[data-cy="body"]').type("TestBody1");
+    cy.get('[data-cy="create-takeaway"]').click();
+    cy.wait("@createTakeaway")
+      .its("request.body")
+      .should("match", /TestTitle1.*TestBody1/);
+  });
 });
