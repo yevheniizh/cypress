ef6c74554f549d753de4131b99b5bdcae6f78c7e
Yevhenii Zhyrov <yevheniizhyrov.dev@gmail.com>
2024-02-20 14:07:45 +0200

    6. Add endpoint test

--- 6/cypress/e2e/newsletter.cy.js
+++ 6/cypress/e2e/newsletter.cy.js
@@ -25,4 +25,16 @@ describe("Newsletter", () => {
     cy.wait("@subscribe");
     cy.contains("Email exists already");
   });
+  it("should successfully create a new contact", () => {
+    cy.request({
+      method: "POST",
+      url: "/newsletter",
+      body: {
+        email: "test3@example.com",
+      },
+      form: true,
+    }).then((response) => {
+      expect(response.status).to.eq(201);
+    });
+  });
 });
