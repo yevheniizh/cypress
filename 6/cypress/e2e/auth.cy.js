af7b85675e316be1dc77de9f47437053b5198342
Yevhenii Zhyrov <yevheniizhyrov.dev@gmail.com>
2024-02-20 14:31:53 +0200

    Minor

--- 6/cypress/e2e/auth.cy.js
+++ 6/cypress/e2e/auth.cy.js
@@ -23,14 +23,7 @@ describe("Auth", () => {
     cy.getCookie("__session").its("value").should("not.be.empty");
   });
   it("should logout", () => {
-    cy.visit("/login");
-    cy.get('[data-cy="auth-email"]').click();
-    cy.get('[data-cy="auth-email"]').type("test@example.com");
-    cy.get('[data-cy="auth-password"]').type("testpassword");
-    cy.get('[data-cy="auth-submit"]').click();
-    cy.location("pathname").should("eq", "/takeaways");
-    cy.getCookie("__session").its("value").should("not.be.empty");
-
+    cy.login();
     cy.contains("Logout").click();
     cy.location("pathname").should("eq", "/");
     cy.getCookie("__session").its("value").should("be.empty");
